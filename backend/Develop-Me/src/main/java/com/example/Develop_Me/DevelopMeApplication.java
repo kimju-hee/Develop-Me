a6b69a171c6423e279c78967f23bab3ffd6f01fb
kimch <rlacogk6577@daum.net>
2024-10-16 18:07:19 +0900

    백엔드 환경설정 및 프로그램 구조 정리

---
+++ backend/Develop-Me/src/main/java/com/example/Develop_Me/DevelopMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Develop_Me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopMeApplication.class, args);
+	}
+
+}
